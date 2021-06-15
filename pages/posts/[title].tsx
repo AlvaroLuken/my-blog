0c77cb913ba561e55923828f63098f30dae8f0a6
AlvaroLuken <al5031@nyu.edu>
2021-06-14 19:43:24 -0700

    my blog start

---
+++ pages/posts/[title].tsx
@@ -0,0 +1,14 @@
+import { Article } from "../../components/Article";
+import { useRouter } from "next/router";
+
+export default function Post() {
+    const router = useRouter();
+    console.log(router.query);
+    const { title } = router.query;
+    return (
+        <Article>
+            <h1>Post Title: {title}</h1>
+            <p>Lorem Ipsum</p>
+        </Article>
+    );
+}
